a55958467bc06820571d6022678f7a5966053e17
Matthew <meswain8@gmail.com>
2024-07-09 19:24:39 +0100

    Continued D3 experimentation

--- dataPlotter.js
+++
@@ -1,9 +0,0 @@
-const el = d3.select("#cpuGraph");
-
-d3.json("ServerManagementDB.HardwareReadings.json").then((data) => {
-    el
-        .selectAll("p")
-        .data(data)
-        .join("p")
-        .text((d) => d.dateTime)
-});
